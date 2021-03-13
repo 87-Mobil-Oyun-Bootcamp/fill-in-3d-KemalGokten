507e64530347d04bfec63adc9424ee713af2c8ee
KemalGokten <ukemalgokten@gmail.com>
2021-03-13 15:10:47 +0300

    Initial Commit

---
+++ Assets/CubeSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private BlockSpawner blockspawner;
+
+    [SerializeField]
+    private GameObject BlocksContainer;
+
+    private int cubesnumbers;
+
+    LevelInfo levelInfo;
+    private bool isStarted = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isStarted)
+        {
+            cubesnumbers = blockspawner.numberofCubes;
+            for(int x=0; x <cubesnumbers; x++)
+            {
+                GameObject cubeObj = Instantiate(BlocksContainer, transform);
+                cubeObj.transform.localPosition = BlocksContainer.transform.position;
+                cubeObj.transform.localScale = new Vector3(0.3f,0.3f,0.3f);
+                cubeObj.GetComponent<Renderer>().material.color = Color.yellow;
+            }
+            isStarted = true;
+        }
+    }
+}
---
+++ Assets/FilledCubes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FilledCubes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public Color RColor;
+   
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ColorCubes"))
+        {
+            Debug.Log("Selam");
+            this.GetComponent<Renderer>().material.color = RColor;
+            this.GetComponent<Collider>().enabled = false;
+            Destroy(other.gameObject);
+
+        }
+    }
+}
